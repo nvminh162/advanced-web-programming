5e89230b989749c7cb9e3a5e5049f34c9390fee6
nvminh162 <nvminh162@gmail.com>
2025-10-03 17:40:07 +0700

    regular: create project spring jpa

---
+++ 02_test/regular/spring-data/nguyenvanminh-22003405-spring-jpa/src/main/java/com/nvminh162/nguyenvanminh/Nguyenvanminh22003405SpringJpaApplication.java
@@ -0,0 +1,13 @@
+package com.nvminh162.nguyenvanminh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nguyenvanminh22003405SpringJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Nguyenvanminh22003405SpringJpaApplication.class, args);
+    }
+
+}
