44d722429d24b54669aa461f0e4d9907a50c3bbb
nvminh162 <nvminh162@gmail.com>
2025-10-03 20:24:51 +0700

    regular: Spring Mongodb initial

---
+++ 02_test/regular/spring-data/nguyenvanminh-22003405-spring-mongodb/src/main/java/com/nvminh162/nguyenvanminh/Nguyenvanminh22003405SpringMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.nvminh162.nguyenvanminh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nguyenvanminh22003405SpringMongodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Nguyenvanminh22003405SpringMongodbApplication.class, args);
+    }
+
+}
